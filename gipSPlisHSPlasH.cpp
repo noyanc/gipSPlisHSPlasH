7df9165422d9e21dc5a1772c1a6208741b144141
KutluT <93340104+KutluT@users.noreply.github.com>
2023-08-03 16:42:41 +0300

    Add files via upload

---
+++ gipSPlisHSPlasH.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "config.h"
+
+int main(int argc, char **argv) {
+	std::cout << "Hello World" << std::endl;
+	std::cout << "Version " << gipSPlisHSPlasH_VERSION_MAJOR << "." << gipSPlisHSPlasH_VERSION_MINOR << std::endl;
+	return 0;
+}
